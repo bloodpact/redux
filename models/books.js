bcd986d1a78ba43c8b0c1077b32063b286819936
Jinxed <toxep@mail.ru>
2018-02-09 17:02:33 +0300

    add db in actions

---
+++ models/books.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var bookSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    images: String,
+    price: Number
+});
+
+var Books = mongoose.model('Books', bookSchema);
+module.exports = Books;
